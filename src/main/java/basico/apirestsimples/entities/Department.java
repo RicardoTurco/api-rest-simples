edb0567d6e6104d59000ef16c0444436b441cef5
RicardoTurco <ricturco@gmail.com>
2023-10-11 10:30:57 -0300

    criando métodos contrutores para departamento

--- src/main/java/basico/apirestsimples/entities/Department.java
+++ src/main/java/basico/apirestsimples/entities/Department.java
@@ -5,6 +5,16 @@ public class Department {
     private Long id;
     private String name;
 
+    public Department() {
+
+    }
+
+    public Department(Long id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
