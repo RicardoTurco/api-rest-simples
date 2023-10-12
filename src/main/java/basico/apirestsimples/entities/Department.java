bfd9edd41ef4b59477a71c4d6fb40e2f393bc453
RicardoTurco <ricturco@gmail.com>
2023-10-12 16:42:28 -0300

    add comentários na entidade Department

--- src/main/java/basico/apirestsimples/entities/Department.java
+++ src/main/java/basico/apirestsimples/entities/Department.java
@@ -5,10 +5,14 @@ public class Department {
     private Long id;
     private String name;
 
+    // Construtor SEM argumentos, para que possamos
+    // instanciar a classe SEM passar as informações "diretamente" ...
     public Department() {
 
     }
 
+    // Construtor COM argumentos, para que possamos
+    // instanciar a classe passando as informações "diretamente" ...
     public Department(Long id, String name) {
         this.id = id;
         this.name = name;
